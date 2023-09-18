66d7f13e9e4aec805d12a4d224a42695ea9cf794
santilin <franciscocapel@cepaim.org>
2023-09-18 15:27:28 +0200

    NotificationTrait.php: added variable and methods to add and get the errors produced by all channels on sending the notification

--- src/NotificationTrait.php
+++ src/NotificationTrait.php
@@ -10,6 +10,9 @@ use yii\helpers\Inflector;
 
 trait NotificationTrait
 {
+    /** @var all the errors produced by the notification channels */
+    private $_notification_errors;
+
     /**
      * @return array
      */
@@ -50,6 +53,53 @@ trait NotificationTrait
             return $this->{$method}();
         }
         throw new \InvalidArgumentException("Can not find message export for chanel `{$channel}`");
+    }
+
+
+    /**
+     * Adds a new error produced by the specified channel.
+     * @param string $channel channel name
+     * @param string $error new error message
+     */
+    public function addError(string $channel, string $err_message)
+    {
+		$this->_notification_errors[$channel] = $err_message;
+    }
+
+    /**
+     * Returns the errors for all channels or a specified channel
+     * @param string|null $channel channel name. Use null to retrieve errors for all channels.
+     * @return array errors for all channels or the specified channel. Empty array is returned if no error.
+     */
+    public function notificationErrors(string $channel = null): array
+    {
+        if ($channel === null) {
+            return $this->_notification_errors === null ? [] : $this->_notification_errors;
+        }
+        return isset($this->_notification_errors[$channel]) ? $this->_notification_errors[$channel] : [];
+    }
+
+    /**
+     * Returns a value indicating whether there is any message sending error.
+     * @param string|null $channel channel name. Use null to check all channels.
+     * @return bool whether there is any error.
+     */
+	public function hasErrors(string $channel = null): bool
+    {
+        return $channel === null ? !empty($this->_notification_errors) : isset($this->_notification_errors[$channel]);
+    }
+
+    /**
+     * Removes errors for all channels or a single channel.
+     * @param string|null $channel channel name. Use null to remove errors for all channels.
+     */
+    public function clearNotificationErrors($channel = null)
+    {
+        if ($channel === null) {
+            $this->_notification_errors = [];
+        } else {
+            unset($this->_notification_errors[$channel]);
+        }
     }
 
 }
