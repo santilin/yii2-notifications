ae26a27c656f6885372d400f2cb2d5fb55ccdb34
Santilín <software@noviolento.es>
2025-05-19 08:56:05 +0200

    DEVEL: mailto and mailfrom

--- src/channels/MailChannel.php
+++ src/channels/MailChannel.php
@@ -103,12 +103,12 @@ class MailChannel extends Component implements ChannelInterface
 			$subject = $this->subjectPrefix . $subject;
 		}
 		if( YII_ENV_DEV ) {
-			if (!isset(Yii::$app->params['develEmail'])) {
-				throw new \Exception("Please, define \$app->params['develEmail']");
+			if (!isset(Yii::$app->params['develEmailFrom']) && !isset(Yii::$app->params['develEmailTo'])) {
+				throw new \Exception("Please, define \$app->params['develEmailTo'] && \$app->params['develEmailFrom']");
 			}
 			$subject = '[dev:to:' . reset($to). "]{$subject}";
-			$message->from = Yii::$app->params['develEmail'];
-			$to = [Yii::$app->params['develEmail']];
+			$message->from = Yii::$app->params['develEmailFrom'];
+			$to = (array)Yii::$app->params['develEmailTo'];
 		}
 		if ($this->viewsPath) {
 			$save_view_path = Yii::$app->mailer->getViewPath();
